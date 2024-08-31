82195c5ac557424986b3f007e9bccda73577ef61
Luk027 <lookman.gohil@gmail.com>
2024-09-01 03:37:52 +0530

    add:vercel.json & fixed Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,45 @@
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
-import { Button } from "./ui/button";
 import {
   SignedIn,
   SignedOut,
-  SignIn,
   UserButton,
+  SignIn,
   useUser,
 } from "@clerk/clerk-react";
-import { BriefcaseBusinessIcon, Heart, PenBox } from "lucide-react";
+import { Button } from "./ui/button";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 
 const Header = () => {
   const [showSignIn, setShowSignIn] = useState(false);
 
   const [search, setSearch] = useSearchParams();
-
   const { user } = useUser();
 
-
   useEffect(() => {
-    if(search.get('sign-in')){
+    if (search.get("sign-in")) {
       setShowSignIn(true);
     }
   }, [search]);
 
   const handleOverlayClick = (e) => {
-    if(e.target === e.currentTarget){
+    if (e.target === e.currentTarget) {
       setShowSignIn(false);
       setSearch({});
     }
-  }
+  };
+
   return (
-    <div className="sticky top-0 z-20 shadow-md  backdrop-blur-lg">
-      <nav className="py-4 flex justify-between items-center container">
+    <>
+      <nav className="container sticky top-0 py-4 flex justify-between items-center shadow backdrop-blur-sm">
         <Link to="/">
           {/* <img src="/logo.png" className="h-20" /> */}
           <span className="self-center text-2xl font-extrabold tracking-widest">
-            <span className='bg-gradient-to-r from-[#4338ca] to-purple-500 bg-clip-text text-transparent font-mono'>PASSION</span>
-            <span className='gradient-title font-mono'>TRACK</span>
-        </span>
+            <span className="bg-gradient-to-r from-[#4338ca] to-purple-500 bg-clip-text text-transparent font-mono">
+              PASSION
+            </span>
+            <span className="gradient-title font-mono">TRACK</span>
+          </span>
         </Link>
 
         <div className="flex gap-8">
@@ -47,32 +48,34 @@ const Header = () => {
               Login
             </Button>
           </SignedOut>
-
           <SignedIn>
-           {user?.unsafeMetadata?.role === "Recruiter" && (
-            <Link to="/post-job">
-              <Button variant="destructive" className="rounded-full">
-                <PenBox size={20} className="mr-2" />
-                Post a job
-              </Button>
-            </Link>
-           )}
-            <UserButton appearance={{
-              elements:{
-                avatarBox:"w-10 h-10",
-              }
-            }}>
+            {user?.unsafeMetadata?.role === "recruiter" && (
+              <Link to="/post-job">
+                <Button variant="destructive" className="rounded-full">
+                  <PenBox size={20} className="mr-2" />
+                  Post a Job
+                </Button>
+              </Link>
+            )}
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
               <UserButton.MenuItems>
                 <UserButton.Link
                   label="My Jobs"
-                  labelIcon={ <BriefcaseBusinessIcon size={15}/> }
+                  labelIcon={<BriefcaseBusiness size={15} />}
                   href="/my-jobs"
                 />
                 <UserButton.Link
                   label="Saved Jobs"
-                  labelIcon={ <Heart size={15}/> }
-                  href="/saved-job"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-jobs"
                 />
+                <UserButton.Action label="manageAccount" />
               </UserButton.MenuItems>
             </UserButton>
           </SignedIn>
@@ -80,16 +83,17 @@ const Header = () => {
       </nav>
 
       {showSignIn && (
-        <div 
-        className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-70 z-10"
-        onClick={handleOverlayClick}>
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-10"
+          onClick={handleOverlayClick}
+        >
           <SignIn
             signUpForceRedirectUrl="/onboarding"
             fallbackRedirectUrl="/onboarding"
           />
         </div>
       )}
-    </div>
+    </>
   );
 };
 
